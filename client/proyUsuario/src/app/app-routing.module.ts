6e51ebb9bdc2a383c809aa51343ddfeb0aad5b6e
Hannyer <hannyer72722@gmail.com>
2021-11-22 20:28:17 -0600

    Nuevo

--- client/proyUsuario/src/app/app-routing.module.ts
+++ client/proyUsuario/src/app/app-routing.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
+import { AddUsuarioComponent } from './components/add-usuario/add-usuario.component';
+import { UsuarioDetailsComponent } from './components/usuario-details/usuario-details.component';
+import { UsuarioListComponent } from './components/usuario-list/usuario-list.component';
+
 import { UsuarioListComponent } from './components/usuario-list/usuario-list.component';
 import { UsuarioDetailsComponent } from './components/usuario-details/usuario-details.component';
 import { AddUsuarioComponent } from './components/add-usuario/add-usuario.component';
